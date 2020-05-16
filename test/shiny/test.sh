5d345dbd84fba1fdbe65ab7b07a5d87cfd4cca8b
Chris Stefano <virtualstaticvoid@gmail.com>
2020-05-16 18:08:20 +0100

    test outputs to file

--- test/shiny/test.sh
+++ test/shiny/test.sh
@@ -3,9 +3,9 @@
 web_url=$(heroku apps:info -j | jq -r '.app.web_url')
 wss_url=${web_url/http/ws}
 
-curl -v ${web_url}
-curl -v ${web_url}shared/shiny.css
-curl -v ${web_url}shared/shiny.min.js
+curl -v ${web_url} -o page.html
+curl -v ${web_url}shared/shiny.css -o tmp.css
+curl -v ${web_url}shared/shiny.min.js -o tmp.js
 
 # test the websocket
-cat test.json | timeout --signal=INT 5 websocat -v -t ${wss_url}websocket/ --
+# cat test.json | timeout --signal=INT 5 websocat -v -t ${wss_url}websocket/ --
